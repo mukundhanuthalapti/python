d9dd7ff3f69cc506f190988999feabee136f2847
mukundha thrisummdhranni <nuthalapatitripura@gmail.com>
2026-03-17 22:37:29 +0530

    Create intro prog 1 .py

---
+++ intro prog 1 .py
@@ -0,0 +1,12 @@
+#Python Input
+name=input("Enter name")#this is input funciton
+#Python Output Statement
+print(name)
+#by default all inputs in string format
+#type casting
+age=int(input("Enter Age"))
+print(age)
+#it returns the given variable data type
+print(type(age))
+            
+        
