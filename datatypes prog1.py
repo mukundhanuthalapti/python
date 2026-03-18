875b4716bf03406ba202fd359e4e2fd08b159baa
mukundha thrisummdhranni <nuthalapatitripura@gmail.com>
2026-03-18 10:32:21 +0530

    Create datatypes prog1.py

---
+++ datatypes prog1.py
@@ -0,0 +1,22 @@
+#if you are assign any data to variables it takes appropriate data type
+code=100
+name="SrinivasaRao"
+age=35
+course="Python"
+fees=3000.50
+is_active=True
+person_id=9999j
+#type function return the data type
+print(type(code))
+print(type(name))
+print(type(age))
+print(type(course))
+print(type(fees))
+print(type(is_active))
+print(type(person_id))
+print(type([1,2,3,4]))
+print(type((1,2,3,4)))
+print(type({1,2,3,4}))
+print(type({"1":1,"2":2,"3":3,"4":4}))
+            
+      
