d267c000dbe29250367a3bec2a3ee8a190ac28e9
mukundha thrisummdhranni <nuthalapatitripura@gmail.com>
2026-03-18 10:36:34 +0530

    Create variable prog1.py

---
+++ variable prog1.py
@@ -0,0 +1,17 @@
+#w.a.p to assing values and print string
+print("Welcome to Python First Program") #this is python output statment
+code = 100
+name = 'SrinivasaRao.K'
+place = "Ongole"
+salary = 45500.50
+# str function can change any format to string format
+strtxt = "My Name is " + name + " and Place is " + place + " MY code is " +\
+       str(code) + " My Salary is " + str(salary)
+print(strtxt)
+print("My Name is ", name, " and Place is ", place, " MY code is ", code ,
+      " My Salary is ", salary)
+print(type(code))
+print(type(name))
+print(type(place))
+print(type(salary))
+
